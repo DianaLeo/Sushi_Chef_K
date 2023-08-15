87d51a2ef44de911ab1b1a512eaa6b8cd15bc3d1
Diana Liu <mata7890@sina.com>
2023-08-15 12:09:01 +1000

    Fixed navigation bar bug

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
+
+
 const navMobileBtn = document.querySelector('.navMobileBtn');
 const navMobileUl = document.querySelector('.navMobile ul');
-const testimonialCards = document.querySelectorAll('.testimonialCard');
-const testimonialsUl = document.querySelector('.testimonialsUl');
+const navMobileBgDim = document.querySelector('.navMobileBgDim');
+
 
 
 navMobileBtn.addEventListener('click', navClickHandler);
 navMobileUl.addEventListener('click', navClickHandler);
 function navClickHandler(){
     navMobileUl.classList.toggle('open');
+    navMobileBgDim.classList.toggle('open');
     if (document.body.style.overflow == 'hidden') {
         document.body.style.overflow = 'scroll'
     } else {
@@ -15,26 +18,3 @@ function navClickHandler(){
     }
 }
 
-const tCardLength = testimonialCards.length;
-const tCardWidth = 370;
-let currentCardID = Math.ceil(tCardLength / 2);
-
-testimonialCards.forEach((card) => {
-    moveCards(card);
-    card.addEventListener('click', () => {
-        currentCardID = card.id;
-        testimonialCards.forEach(card => {
-            moveCards(card);
-        })
-    });
-});
-
-function moveCards(card) {
-    let times = 0.5 + (currentCardID - card.id);
-    card.style.left = `calc(50% - ${tCardWidth * times}px)`;
-    if (card.id != currentCardID) {
-        card.style.opacity = 1 - 0.45 * Math.abs(card.id - currentCardID);
-    } else {
-        card.style.opacity = 1
-    }
-}
---
+++ js/testimonials.js
@@ -0,0 +1,25 @@
+const testimonialCards = document.querySelectorAll('.testimonialCard');
+
+const tCardLength = testimonialCards.length;
+const tCardWidth = 370;
+let currentCardID = Math.ceil(tCardLength / 2);
+
+testimonialCards.forEach((card) => {
+    moveCards(card);
+    card.addEventListener('click', () => {
+        currentCardID = card.id;
+        testimonialCards.forEach(card => {
+            moveCards(card);
+        })
+    });
+});
+
+function moveCards(card) {
+    let times = 0.5 + (currentCardID - card.id);
+    card.style.left = `calc(50% - ${tCardWidth * times}px)`;
+    if (card.id != currentCardID) {
+        card.style.opacity = 1 - 0.45 * Math.abs(card.id - currentCardID);
+    } else {
+        card.style.opacity = 1
+    }
+}
