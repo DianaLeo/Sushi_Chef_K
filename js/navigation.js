3b2c97e64ffd54b7b77a4e6b5ddb6d04b8198b04
Diana Liu <mata7890@sina.com>
2023-08-22 17:35:03 +1000

    Adjusted goto

--- js/navigation.js
+++ js/navigation.js
@@ -1,6 +1,6 @@
 const reservationBtn = document.querySelector('.reservationBtn');
 const reservationWindow = document.querySelector('.reservation');
 
-reservationBtn.addEventListener('click',()=>{
-    reservationWindow.classList.toggle('open');
-})
+// reservationBtn.addEventListener('click',()=>{
+//     reservationWindow.classList.toggle('open');
+// })
