5386c605c346e9b406ebf945fae126b2197cd0e0
Diana Liu <mata7890@sina.com>
2023-09-02 00:42:32 +1000

    Three feature added: Sub menu swipe in, feedback, and google map embedded

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,7 @@
 const menuIntroListItems = document.querySelectorAll('.menuIntroContainer li');
 const menuIntroImgCovers = document.querySelectorAll('.menuIntroImgCover');
+const menuPicturesWrappers = document.querySelectorAll('.menuPicturesWrapper');
+const menuCardWrappers = document.querySelectorAll('.menuCardWrapper');
 
 menuIntroListItems.forEach((li,index)=>{
     const menuIntroImgCover = menuIntroImgCovers[index];
@@ -10,3 +12,23 @@ menuIntroListItems.forEach((li,index)=>{
         menuIntroImgCover.classList.remove('mousehover');
     })
 })
+
+
+window.addEventListener('scroll', showWrappers);
+
+showWrappers();
+
+function showWrappers() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    menuPicturesWrappers.forEach(i => move(i,triggerBottom));
+    menuCardWrappers.forEach(i => move(i,triggerBottom));
+}
+
+function move(i,triggerBottom){
+    const wrapperTop = i.getBoundingClientRect().top
+    if(wrapperTop < triggerBottom) {
+        i.classList.add('show')
+    } else {
+        i.classList.remove('show')
+    }
+}
